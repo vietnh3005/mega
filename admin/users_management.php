6fdddec5ad96f29d25834871e25fe5e7320395f4
vietnh3005 <vietnh3005@gmail.com>
2018-05-02 09:43:01 +0700

    Display user info on modal

--- admin/users_management.php
+++ admin/users_management.php
@@ -32,15 +32,12 @@ $row = mysqli_fetch_assoc($query);
   <link href="css/style.css" rel="stylesheet">
   <link href="css/style-responsive.css" rel="stylesheet" />
 
-  <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
-    <!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-      <script src="js/respond.min.js"></script>
-    <![endif]-->
+
+
+  <script src="js/jquery.js"></script>
   </head>
 
   <body>
-
     <section id="container" class="">
      <!--header start-->
      <?php  include'components/topbar.php'; ?>
@@ -60,7 +57,7 @@ $row = mysqli_fetch_assoc($query);
                   <a class="btn btn-success btn-sm" data-toggle="modal" data-target="#myModal" ><i class="icon-plus"></i> Thêm mới</a> 
                 </div>
               </header>
-              <table class="table table-striped table-advance table-hover">
+              <table class="table table-striped table-advance table-hover" id="dataTable">
                 <thead>
                   <tr>
                     <th><i class=""></i> Tên đăng nhập</th>
@@ -77,152 +74,152 @@ $row = mysqli_fetch_assoc($query);
                 <tbody>
 
                   <?php while($row = mysqli_fetch_assoc($query)) 
-                    { 
-                  ?>
+                  { 
+                    ?>
                     <tr>
-                    <td><a href="#"><?php echo $row['username']?></a></td>
-                    <td class='hidden-phone'><?php echo $row['name']?></td>
-                    <td><?php echo $row['email']?></td>
-                    <td><?php echo $row['phone']?></td>
-                    <td><?php echo $row['address']?></td>
-                    <td><?php echo $row['point']?></td>
-                    <td><?php echo $row['membership_title']?></td>
-                    <td><span class='label label-info label-mini'><?php echo $row['status']?></span></td>
-                    <td> 
-                    <button class='btn btn-success btn-xs'><i class='icon-eye-open' data-toggle='modal' 
-                      data-target='#<?php echo $row['username']?>'></i></button>
-                    <button class='btn btn-primary btn-xs'><i class='icon-pencil'></i></button>
-                    <button class='btn btn-danger btn-xs'><i class='icon-trash'></i></button>
-                    </td>
-                    </tr>
-                    <?php } ?>
-                </tbody>
-              </table>
-            </section>
-          </div>
-        </div>
-        <!-- page end-->
-      </section>
-    </section>
-    <!--main content end-->
-    <!--footer start-->
-    <footer class="site-footer">
-      <div class="text-center">
-        2013 &copy; FlatLab by VectorLab.
-        <a href="#" class="go-top">
-          <i class="icon-angle-up"></i>
-        </a>
-      </div>
-    </footer>
-    <!--footer end-->
-  </section>
-  <!-- js placed at the end of the document so the pages load faster -->
-  <script src="js/jquery.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
-  <script src="js/jquery.scrollTo.min.js"></script>
-  <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-  <script src="js/respond.min.js" ></script>
-
-  <!--common script for all pages-->
-  <script src="js/common-scripts.js"></script>
-
-
-</body>
-</html>
-<!-- Adding Modal -->
-<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title">Thêm mới người dùng</h4>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="col-lg-12">
-            <section class="panel">
-              <div class="panel-body">
-                <div class="form">
-                  <?php  include'components/user_form.php'; ?>
-                </div>
+                      <td><a href="#"><?php echo $row['username']?></a></td>
+                      <td class='hidden-phone'><?php echo $row['name']?></td>
+                      <td><?php echo $row['email']?></td>
+                      <td><?php echo $row['phone']?></td>
+                      <td><?php echo $row['address']?></td>
+                      <td><?php echo $row['point']?></td>
+                      <td><?php echo $row['membership_title']?></td>
+                      <td><span class='label label-info label-mini'><?php echo $row['status']?></span></td>
+                      <td> 
+                        <button class='btn btn-success btn-xs btn_md'><i class='icon-eye-open' data-toggle="modal"
+                           data-id="<?php echo $row['user_id']?>" href="#details_modal"></i></button>
+                          <button class='btn btn-primary btn-xs'><i class='icon-pencil'></i></button>
+                          <button class='btn btn-danger btn-xs'><i class='icon-trash'></i></button>
+                        </td>
+                      </tr>
+                      <?php } ?>
+                    </tbody>
+                  </table>
+                </section>
               </div>
-            </section>
+            </div>
+            <!-- page end-->
+          </section>
+        </section>
+        <!--main content end-->
+        <!--footer start-->
+        <footer class="site-footer">
+          <div class="text-center">
+            2013 &copy; FlatLab by VectorLab.
+            <a href="#" class="go-top">
+              <i class="icon-angle-up"></i>
+            </a>
+          </div>
+        </footer>
+        <!--footer end-->
+      </section>
+      <!-- js placed at the end of the document so the pages load faster -->
+      <!-- <script src="js/jquery.js"></script> -->
+      <script src="js/bootstrap.min.js"></script>
+      <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+      <script src="js/jquery.scrollTo.min.js"></script>
+      <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+      <script src="js/respond.min.js" ></script>
+
+      <!--common script for all pages-->
+      <script src="js/common-scripts.js"></script>
+
+
+    </body>
+    </html>
+    <!-- Adding Modal -->
+    <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title">Thêm mới người dùng</h4>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-lg-12">
+                <section class="panel">
+                  <div class="panel-body">
+                    <div class="form">
+                      <?php  include'components/user_form.php'; ?>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-</form>
-<!-- modal -->
+  </form>
+  <!-- modal -->
 
+  <!-- User Details Modal -->
+  <div class="modal" id="details_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="text-danger fa fa-times"></i></button>
+          <h4 class="modal-title" id="myModalLabel"><i class="text-muted fa fa-shopping-cart"></i> <strong>Tài khoản</strong> - <?php echo $row['name']; ?> </h4>
+        </div>
+        <div class="modal-body">
 
-<!-- User Details Modal -->
-<div class="modal" id="<?php echo $row['usser_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="text-danger fa fa-times"></i></button>
-        <h4 class="modal-title" id="myModalLabel"><i class="text-muted fa fa-shopping-cart"></i> <strong>Tài khoản</strong> - <?php echo $row['user_id']; ?> </h4>
-      </div>
-      <div class="modal-body">
+          <table class="pull-left col-md-8 ">
+           <tbody>
 
-        <table class="pull-left col-md-8 ">
-         <tbody>
-           <tr>
-             <td class="h6"><strong>Código</strong></td>
+            <tr>
+             <td class="h6"><strong>ID</strong></td>
              <td> </td>
-             <td class="h5">02051</td>
+             <td class="h5" id="user_id"></td>
            </tr>
 
            <tr>
-             <td class="h6"><strong>Descrição</strong></td>
+             <td class="h6"><strong>Tên đăng nhập</strong></td>
              <td> </td>
              <td class="h5">descrição do produto</td>
            </tr>
 
            <tr>
-             <td class="h6"><strong>Marca/Fornecedor</strong></td>
+             <td class="h6"><strong>Tên</strong></td>
              <td> </td>
              <td class="h5">Marca do produto</td>
            </tr>
 
            <tr>
-             <td class="h6"><strong>Número Original</strong></td>
+             <td class="h6"><strong>Email</strong></td>
              <td> </td>
              <td class="h5">0230316</td>
            </tr>
 
            <tr>
-             <td class="h6"><strong>Código N.C.M</strong></td>
+             <td class="h6"><strong>Số điện thoại</strong></td>
              <td> </td>
              <td class="h5">032165</td>
            </tr>
 
            <tr>
-             <td class="h6"><strong>Código de Barras</strong></td>
+             <td class="h6"><strong>Địa chỉ</strong></td>
              <td> </td>
              <td class="h5">0321649843</td>
            </tr>  
 
            <tr>
-             <td class="h6"><strong>Unid. por Embalagem</strong></td>
+             <td class="h6"><strong>Điểm</strong></td>
              <td> </td>
              <td class="h5">50</td>
            </tr>                            
 
            <tr>
-             <td class="h6"><strong>Quantidade Mínima</strong></td>
+             <td class="h6"><strong>Thành viên</strong></td>
              <td> </td>
              <td class="h5">50</td>
            </tr>
 
            <tr>
-             <td class="h6"><strong>Preço Atacado</strong></td>
+             <td class="h6"><strong>Trạng thái</strong></td>
              <td> </td>
              <td class="h5">R$ 35,00</td>
-           </tr> 
+           </tr>
 
            <tr>
              <td class="btn-mais-info text-primary">
@@ -240,7 +237,6 @@ $row = mysqli_fetch_assoc($query);
        <div class="col-md-4"> 
         <img src="#" alt="teste" class="img-thumbnail">  
       </div>
-
       <div class="clearfix"></div>
       <p class="open_info hide">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
     </div>
@@ -261,7 +257,7 @@ $row = mysqli_fetch_assoc($query);
   </div>
 </div>
 </div>
-<!-- fim Modal-->    
+<!-- fim Modal-->
 
 <!-- Auto dismiss alert-->
 <script type="text/javascript">
@@ -270,4 +266,20 @@ $row = mysqli_fetch_assoc($query);
       $(this).remove(); 
     });
   }, 2000);
+</script>
+
+<!-- Open Modal -->
+<script>
+  $(document).ready(function(){
+    $(".btn_md").click(function(){
+      $("#details_modal").modal();
+    });
+  });
+</script>
+
+<script type="text/javascript">
+  $(document).on("click",".btn_md", function(){
+    var user_id = $(this).data('id');
+    $(".modal-body #user_id").val(user_id);
+  });
 </script>
